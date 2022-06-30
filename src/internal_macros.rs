41eddb98df946daaf77289ae6b8781cbbe282110
Tobin C. Harding <me@tobin.cc>
2022-06-30 14:33:51 +1000

    Remove unused lifetime
    Clippy emits:
    
     warning: this lifetime isn't used in the impl
    
    Remove unused lifetime.

--- src/internal_macros.rs
+++ src/internal_macros.rs
@@ -47,7 +47,7 @@ macro_rules! serde_string_impl {
 		}
 
 		#[cfg(feature = "serde")]
-		impl<'de> $crate::serde::Serialize for $name {
+		impl $crate::serde::Serialize for $name {
 			fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
 			where
 				S: $crate::serde::Serializer,
