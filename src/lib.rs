a083fafb69fdafddb84b81ab85cc07c5a2f0ec80
Tobin C. Harding <me@tobin.cc>
2022-06-30 14:50:57 +1000

    Use hyperlink
    Building the docs throws a warning:
    
     warning: this URL is not a hyperlink
    
    As suggested, use a hyperlink.

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@
 
 //! # BIP39 Mnemonic Codes
 //!
-//! https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki
+//! <https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki>
 //!
 
 #![deny(non_upper_case_globals)]
