c7ae12fb647e039abda2dc9b074fde7431051a9d
pezcore <tvboyd23@gmail.com>
2023-03-24 13:22:53 -0400

    doc: enable documentation of feature-gated items in docsrs

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,7 @@
 #![deny(unused_imports)]
 #![deny(missing_docs)]
 #![cfg_attr(all(not(test), not(feature = "std")), no_std)]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 
 #[cfg(any(test, feature = "std"))]
 pub extern crate core;
